dce62531f258c79a4efd68ac10b2c78d6db03786
Pipluppp <duncanb013@gmail.com>
2024-02-23 00:21:57 +0800

    Implement working Hyponyms for k!=0
    Written solely in the HyponymsHandler (not that clean and modular), but passes manual comparisons with Staff Solution

--- proj2b/ngordnet/main/HyponymsHandler.java
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -2,11 +2,83 @@ package ngordnet.main;
 
 import ngordnet.browser.NgordnetQuery;
 import ngordnet.browser.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import ngordnet.ngrams.TimeSeries;
+
+import java.util.*;
 
 public class HyponymsHandler extends NgordnetQueryHandler {
+    Graph dag;
+    NGramMap map;
+
+    public HyponymsHandler(Graph g, NGramMap m) {
+        this.dag = g;
+        this.map = m;
+    }
+
     @Override
     public String handle(NgordnetQuery q) {
-        String response = "Hello";
+        String response = "";
+        List<String> words = q.words();
+        HashSet<String> setOfHyponyms;
+
+        // Find set of hyponyms
+        if (words.size() == 1) {
+            setOfHyponyms = dag.hyponyms(words.get(0));
+        } else {
+            String[] arrayWords = words.toArray(new String[0]);
+            setOfHyponyms = dag.hyponyms(arrayWords);
+        }
+
+        // If k != 0
+        Map<String, Double> hyponymFrequency = new TreeMap<>();
+        HashSet<String> temp = new HashSet<>();
+        if (q.k() != 0){
+            // Load words into map with its NGramMap sum of frequency
+            for (String word: setOfHyponyms) {
+                TimeSeries wordTS = map.countHistory(word, q.startYear(), q.endYear());
+                if (wordTS.isEmpty()) {
+                    continue;
+                }
+
+                Double sum = 0.0;
+                for (Map.Entry<Integer, Double> entry: wordTS.entrySet()) {
+                    sum += entry.getValue();
+                }
+                if (sum != 0.0) {
+                    hyponymFrequency.put(word, sum);
+                }
+            }
+
+            // Store Top k frequencies and sort (but in ascending order, just index in reverse to get descending)
+            ArrayList<Double> sortedSum = new ArrayList<>();
+            for (Map.Entry<String, Double> entry: hyponymFrequency.entrySet()) {
+                sortedSum.add(entry.getValue());
+            }
+            Collections.sort(sortedSum);
+
+            // For the given top frequency, find the mapping in our TimeSeries (to get the word)
+            for (int i = 0; i < q.k(); i++) {
+                if (i == hyponymFrequency.size()) {
+                    break;
+                }
+
+                // Search for the key (word) which has the value
+                for (Map.Entry<String, Double> entry: hyponymFrequency.entrySet()) {
+                    Long a = Math.round(entry.getValue());
+                    Long b = Math.round(sortedSum.get(sortedSum.size() - i - 1));
+                    if (a.equals(b)) {
+                        temp.add(entry.getKey());
+                    }
+                }
+            }
+            // Update set of hyponyms
+            setOfHyponyms = temp;
+        }
+
+        // Sort hyponyms
+        List<String> sorted = dag.sortHyponyms(setOfHyponyms);
+        response += sorted;
         return response;
     }
 }
--- proj2b/ngordnet/main/Main.java
+++ proj2b/ngordnet/main/Main.java
@@ -6,17 +6,19 @@ import ngordnet.ngrams.NGramMap;
 public class Main {
     public static void main(String[] args) {
         NgordnetServer hns = new NgordnetServer();
-        String wordFile = "./data/ngrams/top_49887_words.csv";
+        String wordFile = "./data/ngrams/top_14377_words.csv";
         String countFile = "./data/ngrams/total_counts.csv";
 
         String synsetFile = "./data/wordnet/synsets.txt";
         String hyponymFile = "./data/wordnet/hyponyms.txt";
 
-        NGramMap ngm = new NGramMap(wordFile, countFile);
+        Graph graph = new Graph(synsetFile, hyponymFile);
+        NGramMap ngram = new NGramMap(wordFile, countFile);
+        // NGramMap ngm = new NGramMap(wordFile, countFile);
 
         hns.startUp();
-        hns.register("history", new HistoryHandler(ngm));
-        hns.register("historytext", new HistoryTextHandler(ngm));
-        hns.register("hyponyms", new HyponymsHandler());
+        hns.register("history", new HistoryHandler(ngram));
+        hns.register("historytext", new HistoryTextHandler(ngram));
+        hns.register("hyponyms", new HyponymsHandler(graph, ngram));
     }
 }
